4582e6947eb4298e5ff8959eb745c67543267cb9
Lucas Mathioni <mathioni@me.com>
2022-05-12 09:19:12 -0300

    :sparkles: useValidation hook

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export * from './@types';
+export * from './react';
 export * from './validators';
---
+++ src/react/hooks/useValidation.hook.ts
@@ -0,0 +1,45 @@
+import type {
+  ValidationConfig,
+  ValidationData,
+  ValidationFields,
+  ValidationSchema,
+  ValidationType,
+} from '@types';
+
+import { useCallback, useMemo } from 'react';
+
+import { validateField, validateFields } from '@validators';
+
+export function useValidation<
+  TData extends ValidationData,
+  TSchema extends ValidationSchema = ValidationSchema,
+>(schema: TSchema) {
+  const handleValidateField = useCallback(
+    function <T extends keyof TData>(
+      field: keyof typeof schema,
+      value: TData[T] | unknown,
+    ): string | undefined {
+      const fieldSchema = schema[field];
+      return validateField(
+        fieldSchema as ValidationConfig,
+        value as ValidationType,
+      );
+    },
+    [schema],
+  );
+
+  const handleValidateFields = useCallback(
+    function (data: TData): ValidationFields | undefined {
+      return validateFields(schema, data);
+    },
+    [validateField],
+  );
+
+  return useMemo(
+    () => ({
+      validateField: handleValidateField,
+      validateFields: handleValidateFields,
+    }),
+    [handleValidateField, handleValidateFields],
+  );
+}
